b6a9709fcd2d8cdcc29de216a99ec61e3dadee04
Xzenooo <e.jouny@lprs.fr>
2022-05-17 19:33:40 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module projet.bowling {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens projet.bowling to javafx.fxml;
+    exports projet.bowling;
+}
