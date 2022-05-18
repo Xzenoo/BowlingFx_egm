45102f48ef96ff6a0ff0ac7e57c4c764621c5452
BRUSSON_Gr <brusgregory@gmail.com>
2022-05-18 16:17:24 +0200

    Refactor : Ajout Attribut

--- src/main/java/modele/NiveauJeu.java
+++ src/main/java/modele/NiveauJeu.java
@@ -1,4 +1,23 @@
 package modele;
 
 public class NiveauJeu {
+    private int IdNiveauJeu;
+    private String nom;
+
+
+    public int getIdNiveauJeu() {
+        return IdNiveauJeu;
+    }
+
+    public void setIdNiveauJeu(int idNiveauJeu) {
+        IdNiveauJeu = idNiveauJeu;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
