7dc60d224d9558a1588f64d3c0bf41bde17c3bea
Xzenoo <e.jouny@lprs.fr>
2022-05-18 16:17:42 +0200

    refacror : attribut Catégorie

--- src/main/java/modele/Catégorie.java
+++ src/main/java/modele/Catégorie.java
@@ -1,4 +1,10 @@
 package modele;
 
 public class Catégorie {
+
+    // ===Attribut===
+    private int id_Categorie;
+    private String libelle;
+    private int ageMin;
+    private int ageMax;
 }
