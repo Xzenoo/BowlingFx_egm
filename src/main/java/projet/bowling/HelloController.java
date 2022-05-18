8fd3989cb41b08ed334ebad59d330d5edd2a25e3
Xzenoo <e.jouny@lprs.fr>
2022-05-18 15:52:58 +0200

    refactor : creation de package modele, et repository ainsi que la class User

--- src/main/java/projet/bowling/HelloController.java
+++ src/main/java/projet/bowling/HelloController.java
@@ -1,14 +1,21 @@
 package projet.bowling;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label LblErreur;
+
+    @FXML
+    void BtnConnexion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BtnInscription(ActionEvent event) {
+
     }
 }
