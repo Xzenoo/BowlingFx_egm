d217b5e8201bc8185d7e4ac15f787b890be007c8
Xzenoo <e.jouny@lprs.fr>
2022-05-18 16:14:47 +0200

    refacror : attribut Pratiquant

--- src/main/java/modele/Pratiquant.java
+++ src/main/java/modele/Pratiquant.java
@@ -1,4 +1,12 @@
 package modele;
 
 public class Pratiquant {
+
+//      ===attribut===
+    private int id_Pratiquant;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String email;
+    private String mdp;
 }
